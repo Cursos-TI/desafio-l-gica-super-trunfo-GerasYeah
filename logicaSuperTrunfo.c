02db372a0383c44aeeb2e842e51115be685095a5
GerasYeah <57162490+GerasYeah@users.noreply.github.com>
2025-02-02 02:32:10 +0000

    Cadastro e comparação das cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,36 +1,103 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    // Variáveis para armazenar as propriedades das cartas
 
+    char codigoA[4], estadoA[3], cidadeA[31], codigoB[4], estadoB[3], cidadeB[31];
+    float pibA, pibB, areaA, areaB;
+    int populacaoA, populacaoB, pontosTuristicosA, pontosTuristicosB;
+
+    // Variáveis para armazenar as informações da comparação das cartas
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    char cidadeVencedora[31];
+    int pontosCartaA, pontosCartaB;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    // Cadastro da carta 1
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    printf("Digite o código da carta: ");
+    scanf("%3s", codigoA);
+   
+    printf("Digite a sigla do Estado: ");
+    scanf("%2s", estadoA);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("Digite o nome da cidade: ");
+    scanf("%30s", cidadeA);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacaoA);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pibA);
+
+    printf("Digite a area: ");
+    scanf("%f", &areaA);
+
+    printf("Digite o total de pontos turísticos: ");
+    scanf("%d", &pontosTuristicosA);
+
+    // Exibindo as propriedades da carta 1
+    printf("Informações da carta 1:\n");
+    printf("Código: %s\n", codigoA);
+    printf("Estado: %s\n", estadoA);
+    printf("Cidade: %s\n", cidadeA);
+    printf("População: %d\n", populacaoA);
+    printf("Área: %f\n", areaA);
+    printf("PIB: %f\n", pibA);
+    printf("Área: %d\n\n", populacaoA);
+
+    // Cadastro da carta 2
+
+    printf("Digite o código da carta 2: ");
+    scanf("%3s", codigoB);
+   
+    printf("Digite a sigla do Estado: ");
+    scanf("%2s", estadoB);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%30s", cidadeB);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacaoB);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pibB);
+
+    printf("Digite a area: ");
+    scanf("%f", &areaB);
+
+    printf("Digite o total de pontos turísticos: ");
+    scanf("%d", &pontosTuristicosB);
+
+    // Exibindo as propriedades da carta 2
+    printf("Informações da carta 2:\n");
+    printf("Código: %s\n", codigoB);
+    printf("Estado: %s\n", estadoB);
+    printf("Cidade: %s\n", cidadeB);
+    printf("População: %d\n", populacaoB);
+    printf("Área: %f\n", areaB);
+    printf("PIB: %f\n", pibB);
+    printf("Área: %d\n\n", populacaoB);
+
+    // Comparação de cartas
+    if (populacaoA > populacaoB) {
+        printf("Cidade 1 tem maior população.\n");
+        pontosCartaA++;
+    } else {
+         printf("Cidade 2 tem maior população.\n");
+         pontosCartaB++;
+    }
+
+    if (areaA > areaB) {
+        printf("Cidade 1 tem um território maior.\n");
+        pontosCartaA++;
+    } else {
+         printf("Cidade 2 tem um território maior.\n");
+         pontosCartaB++;
+    }
+
+
 
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
